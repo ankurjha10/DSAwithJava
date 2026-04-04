19cea158613b5f882e39a63a6cdf31202d63d688
Ankur Jha <ankurjha521@gmail.com>
2026-04-04 13:40:41 +0530

    SearchInRange

---
+++ src/LinearSearch/SearchInRange.java
@@ -0,0 +1,22 @@
+package LinearSearch;
+
+public class SearchInRange {
+    static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int target = 5;
+        System.out.println(search(arr, target, 2, 8));
+    }
+
+    static int search(int[] arr, int target, int start, int end){
+
+        if (arr.length == 0)
+            return -1;
+
+        for (int i = start; i<= end; i++){
+
+            if (arr[i] == target)
+                return i;
+        }
+        return -1;
+    }
+}
