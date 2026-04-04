24f335289349e90874902b84ee06caa15a8e1ae2
Ankur Jha <ankurjha521@gmail.com>
2026-04-04 14:03:39 +0530

    FindMin

---
+++ src/LinearSearch/FindMin.java
@@ -0,0 +1,21 @@
+package LinearSearch;
+
+public class FindMin {
+    static void main(String[] args) {
+        int[] arr = {119, 37, 118, 78, 46, 68, 21};
+        System.out.println(MIN(arr));
+    }
+
+    static int MIN(int[] arr){
+        if (arr.length == 0)
+            return -1;
+
+        int ans = arr[0];
+        for(int i = 1; i<arr.length; i++){
+
+            if (arr[i] < ans)
+                ans = arr[i];
+        }
+        return ans;
+    }
+}
