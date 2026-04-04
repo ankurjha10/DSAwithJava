60c8a9a47a9406666a9bcacae32bb423ffd7d95b
Ankur Jha <ankurjha521@gmail.com>
2026-04-04 16:01:06 +0530

    SearchIn2DArray

---
+++ src/LinearSearch/SearchIn2DArray.java
@@ -0,0 +1,29 @@
+package LinearSearch;
+
+import java.util.Arrays;
+
+public class SearchIn2DArray {
+    static void main(String[] args) {
+        int[][] arr = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+
+        int target = 99;
+
+        int[] search = search(arr, target);
+        System.out.println(Arrays.toString(search));
+    }
+
+    static int[] search(int[][] arr, int target){
+
+        for (int i = 0; i<arr.length; i++){
+            for (int j = 0; j<arr[i].length; j++){
+                if (arr[i][j] == target)
+                    return new int[]{i, j};
+            }
+        }
+        return new int[]{-1, -1};
+    }
+}
