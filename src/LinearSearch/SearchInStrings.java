3731d151c9d2eb43a496c546ad9cbb0ba66fd0a8
Ankur Jha <ankurjha521@gmail.com>
2026-04-04 13:21:22 +0530

    SearchInStrings

---
+++ src/LinearSearch/SearchInStrings.java
@@ -0,0 +1,24 @@
+package LinearSearch;
+
+public class SearchInStrings {
+    public static void main(String[] args) {
+        String name = "Ankur";
+        char target = '1';
+
+        System.out.println(search(name, target));
+
+    }
+
+    static boolean search(String str, char target){
+
+        if (str.isEmpty())
+            return false;
+
+        for(int i = 0; i<str.length(); i++){
+
+            if (target == str.charAt(i))
+                return true;
+        }
+        return false;
+    }
+}
