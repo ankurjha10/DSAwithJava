da44b3161683b61f5325a959793369088855ab22
Ankur Jha <ankurjha521@gmail.com>
2026-04-05 20:59:28 +0530

    ReverseArray

---
+++ src/Arrays/ReverseArray.java
@@ -0,0 +1,23 @@
+package Arrays;
+
+import java.util.Arrays;
+
+public class ReverseArray {
+    static void main() {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        reverse(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void reverse(int[] arr){
+
+        int start = 0;
+        int end = arr.length-1;
+
+        while(start<end){
+            Swap.swap(arr, start, end);
+            start++;
+            end--;
+        }
+    }
+}
