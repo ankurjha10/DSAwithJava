c269dad38719499262080c43eac19f328a7fce8b
Ankur Jha <ankurjha521@gmail.com>
2026-04-05 20:21:46 +0530

    ArrayListExample

---
+++ src/Arrays/ArrayListExample.java
@@ -0,0 +1,14 @@
+package Arrays;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ArrayListExample {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        ArrayList<Integer> arrayList = new ArrayList<>();
+
+
+    }
+}
